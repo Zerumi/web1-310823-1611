9ebc09096ae0a6363e698ed4d3b3257164265b9a
Zerumi <afanasyev09032005@gmail.com>
2023-09-01 00:34:42 +0300

    feat: form handling

---
+++ actions/check-hit.php
@@ -0,0 +1,8 @@
+<html lang="en">
+    <body>
+        Data: <br/>
+        X: <?php echo $_GET["x-select"]; ?> <br/>
+        Y: <?php echo $_GET["y-select"]; ?> <br/>
+        Z: <?php echo $_GET["z-select"]; ?>
+    </body>
+</html>
